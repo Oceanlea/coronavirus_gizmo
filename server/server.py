a03e9eaeaba16d35a6f5b0234f081250bff21e8f
Mr. Amazing <andrewadiletta@gmail.com>
2020-01-28 00:02:15 -0500

    Create server.py

---
+++ server/server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+
+PORT = <Your port>
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
